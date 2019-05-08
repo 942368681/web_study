fbe7eea599e2b56a4e6c24b35988681539f4c8e2
Sfl <942368681@qq.com>
2019-05-08 23:04:47 +0800

    update

---
+++ vue/vue-test-2/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>|
+      <router-link to="/login" v-if="!isLogin">Login</router-link>
+      <span>{{loginState}}</span>
+    </div>
+
+    <div>{{count}}
+      <button @click="onAdd">add</button>
+    </div>
+
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from "vuex";
+
+import store from './kstore';
+
+export default {
+  computed: {
+    ...mapState(["isLogin"]),
+    ...mapGetters(["loginState"]),
+    count(){
+      return store.state.count;
+    }
+  },
+  methods: {
+    onAdd() {
+      store.commit('add')
+    }
+  },
+};
+</script>
+
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
