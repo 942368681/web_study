b487cc88ca4e84cde1543c999247f12737fce4ee
Sfl <942368681@qq.com>
2019-08-13 23:16:05 +0800

    s

--- node/node02/sample/index.js
+++ node/node02/sample/index.js
@@ -7,6 +7,16 @@ const app = new Koa()
 //         }
 //     ]
 //     next()
+    
+//     ctx.body.push({
+//         name: 'sfl'
+//     })
+// })
+
+// app.use((ctx, next) => {
+//     ctx.body.push({
+//         name: 'jerry'
+//     })
 // })
 
 // const router = {}
@@ -25,25 +35,26 @@ const app = new Koa()
 //     console.log('url' + ctx.url)
 //     router[ctx.url](ctx)
 // })
-app.use(async (ctx,next) => {
-    const start = new Date().getTime()
-    console.log(`start: ${ctx.url}`);
-    await next();s
-    const end = new Date().getTime()
-    console.log(`请求${ctx.url}, 耗时${parseInt(end-start)}ms`)
-})
 
-app.use(require('koa-static')(__dirname + '/'))
-const router = require('koa-router')()
-router.get('/string',async (ctx,next) => {
-    ctx.body = 'koa2 string'
-})
-router.get('/json',async (ctx,next) => {
-    ctx.body = {
-        title: 'koa2 json'
-    }
-})
-app.use(router.routes())
+// app.use(async (ctx,next) => {
+//     const start = new Date().getTime()
+//     console.log(`start: ${ctx.url}`);
+//     await next();
+//     const end = new Date().getTime()
+//     console.log(`请求${ctx.url}, 耗时${parseInt(end-start)}ms`)
+// })
+
+app.use(require('koa-static')(__dirname + '/www'))
+// const router = require('koa-router')()
+// router.get('/string',async (ctx,next) => {
+//     ctx.body = 'koa2 string'
+// })
+// router.get('/json',async (ctx,next) => {
+//     ctx.body = {
+//         title: 'koa2 json'
+//     }
+// })
+// app.use(router.routes())
 
 
 app.listen(3000)
