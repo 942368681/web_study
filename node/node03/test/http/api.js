f3bbc24b2304730ca7ca9401a8f08d811c45010d
Sfl <942368681@qq.com>
2019-08-31 16:36:36 +0800

    http

---
+++ node/node03/test/http/api.js
@@ -0,0 +1,30 @@
+// http.js
+const http = require("http");
+const fs = require("fs");
+
+const app = http
+    .createServer((req, res) => {
+        const {
+            method,
+            url
+        } = req;
+        if (method == "GET" && url == "/") {
+            fs.readFile("./index.html", (err, data) => {
+                res.setHeader("Content-Type", "text/html");
+                res.end(data);
+            });
+
+
+
+        } else if (method == "GET" && url == "/api/users") {
+            res.setHeader("Content-Type", "application/json");
+
+            res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+
+            res.end(JSON.stringify([{
+                name: "tom",
+                age: 20
+            }]));
+        }
+    })
+module.exports = app
