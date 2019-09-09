58cab057c2632acb6469900136f99a48d596c188
Sfl <942368681@qq.com>
2019-09-09 14:23:59 +0800

    update

--- node/node05/mongo/helloworld.js
+++ node/node05/mongo/helloworld.js
@@ -5,15 +5,19 @@ setTimeout(async () => {
 
     const { MongoClient: MongoDB } = require('mongodb')
     const client = new MongoDB(
-        'mongodb://localhost:27017',
+        'mongodb://localsdhost:27017',
         {
             useNewUrlParser: true
         }
     )
-    let ret
     // 创建连接
-    ret = await client.connect()
-    const db = client.db('test')
+    client.connect((err, client) => {
+        console.log(err);
+        if (err) throw err;
+        console.log(client);
+    })
+    console.log(111111);
+    // const db = client.db('test')
 
     // const fruits = db.collection('fruits')
     // ret = await fruits.insertOne({
@@ -36,29 +40,29 @@ setTimeout(async () => {
 
     // await fruits.deleteMany()
 
-    const stations = db.collection("stations");
+    // const stations = db.collection("stations");
     // 添加测试数据，执行一次即可
-    await stations.insertMany([
-        { name: "天安门东", loc: [116.407851, 39.91408] },
-        { name: "天安门西", loc: [116.398056, 39.913723] },
-        { name: "王府井", loc: [116.417809, 39.91435] }
-    ]);
-    await stations.createIndex({ loc: "2dsphere" });
-    r = await stations.find({
-        loc: {
-            $nearSphere: {
-                $geometry: {
-                    type: "Point",
-                    coordinates: [116.403847, 39.915526]
-                },
-                $maxDistance: 1000
-            }
-        }
-    }).toArray();
-    console.log("天安门附近地铁站", r);
+    // await stations.insertMany([
+    //     { name: "天安门东", loc: [116.407851, 39.91408] },
+    //     { name: "天安门西", loc: [116.398056, 39.913723] },
+    //     { name: "王府井", loc: [116.417809, 39.91435] }
+    // ]);
+    // await stations.createIndex({ loc: "2dsphere" });
+    // r = await stations.find({
+    //     loc: {
+    //         $nearSphere: {
+    //             $geometry: {
+    //                 type: "Point",
+    //                 coordinates: [116.403847, 39.915526]
+    //             },
+    //             $maxDistance: 1000
+    //         }
+    //     }
+    // }).toArray();
+    // console.log("天安门附近地铁站", r);
 
 
-    client.close()
+    // client.close()
 
 
 })
